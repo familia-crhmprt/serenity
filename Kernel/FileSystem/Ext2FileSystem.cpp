familia-crhmprt/serenity

commit df66c28479e9206adeef1f40f8c0e448c8aea77e

Kernel: Deemphasize inode identifiers
These APIs were clearly modeled after Ext2FS internals, and make perfect sense
in Ext2FS context. The new APIs are more generic, and map better to the
semantics exported to the userspace, where inode identifiers only appear in
stat() and readdir() output, but never in any input.

This will also hopefully reduce the potential for races (see commit https://github.com/SerenityOS/serenity/commit/c44b4d61f350703fcf1bbd8f6e353b9c6c4210c2).

Lastly, this makes it way more viable to implement a filesystem that only
synthesizes its inodes lazily when queried, and destroys them when they are no
longer in use. With inode identifiers being used to reference inodes, the only
choice for such a filesystem is to persist any inode it has given out the
identifier for, because it might be queried at any later time. With direct
references to inodes, the filesystem will know when the last reference is
dropped and the inode can be safely destroyed.

Kernel/FileSystem/Ext2FileSystem.cpp
@@ -150,9 +150,9 @@ const char* Ext2FS::class_name() const
     return "Ext2FS";
 }
 
-InodeIdentifier Ext2FS::root_inode() const
+NonnullRefPtr<Inode> Ext2FS::root_inode() const
 {
-    return { fsid(), EXT2_ROOT_INO };
+    return *get_inode({ fsid(), EXT2_ROOT_INO });
 }
 
 bool Ext2FS::find_block_containing_inode(unsigned inode, unsigned& block_index, unsigned& offset) const
@@ -935,7 +935,14 @@ bool Ext2FSInode::write_directory(const Vector<FS::DirectoryEntry>& entries)
     return static_cast<size_t>(nwritten) == directory_data.size();
 }
 
-KResult Ext2FSInode::add_child(InodeIdentifier child_id, const StringView& name, mode_t mode)
+KResultOr<NonnullRefPtr<Inode>> Ext2FSInode::create_child(const String& name, mode_t mode, dev_t dev, uid_t uid, gid_t gid)
+{
+    if (mode & S_IFDIR)
+        return fs().create_directory(identifier(), name, mode, uid, gid);
+    return fs().create_inode(identifier(), name, mode, 0, dev, uid, gid);
+}
+
+KResult Ext2FSInode::add_child(Inode& child, const StringView& name, mode_t mode)
 {
     LOCKER(m_lock);
     ASSERT(is_directory());
@@ -944,7 +951,7 @@ KResult Ext2FSInode::add_child(InodeIdentifier child_id, const StringView& name,
         return KResult(-ENAMETOOLONG);
 
 #ifdef EXT2_DEBUG
-    dbg() << "Ext2FSInode::add_child(): Adding inode " << child_id.index() << " with name '" << name << "' and mode " << mode << " to directory " << index();
+    dbg() << "Ext2FSInode::add_child(): Adding inode " << child.index() << " with name '" << name << "' and mode " << mode << " to directory " << index();
 #endif
 
     Vector<FS::DirectoryEntry> entries;
@@ -966,17 +973,14 @@ KResult Ext2FSInode::add_child(InodeIdentifier child_id, const StringView& name,
         return KResult(-EEXIST);
     }
 
-    auto child_inode = fs().get_inode(child_id);
-    if (child_inode) {
-        result = child_inode->increment_link_count();
-        if (result.is_error())
-            return result;
-    }
+    result = child.increment_link_count();
+    if (result.is_error())
+        return result;
 
-    entries.empend(name.characters_without_null_termination(), name.length(), child_id, to_ext2_file_type(mode));
+    entries.empend(name.characters_without_null_termination(), name.length(), child.identifier(), to_ext2_file_type(mode));
     bool success = write_directory(entries);
     if (success)
-        m_lookup_cache.set(name, child_id.index());
+        m_lookup_cache.set(name, child.index());
     return KSuccess;
 }
 
@@ -1405,11 +1409,6 @@ KResultOr<NonnullRefPtr<Inode>> Ext2FS::create_inode(InodeIdentifier parent_id, 
         return KResult(-ENOSPC);
     }
 
-    // Try adding it to the directory first, in case the name is already in use.
-    auto result = parent_inode->add_child({ fsid(), inode_id }, name, mode);
-    if (result.is_error())
-        return result;
-
     auto blocks = allocate_blocks(group_index_from_inode(inode_id), needed_blocks);
     ASSERT(blocks.size() == needed_blocks);
 
@@ -1458,6 +1457,10 @@ KResultOr<NonnullRefPtr<Inode>> Ext2FS::create_inode(InodeIdentifier parent_id, 
     auto inode = get_inode({ fsid(), inode_id });
     // If we've already computed a block list, no sense in throwing it away.
     static_cast<Ext2FSInode&>(*inode).m_block_list = move(blocks);
+
+    auto result = parent_inode->add_child(*inode, name, mode);
+    ASSERT(result.is_success());
+
     return inode.release_nonnull();
 }
 

Kernel/FileSystem/DevPtsFS.cpp
@@ -75,19 +75,9 @@ static unsigned pty_index_to_inode_index(unsigned pty_index)
     return pty_index + 2;
 }
 
-InodeIdentifier DevPtsFS::root_inode() const
+NonnullRefPtr<Inode> DevPtsFS::root_inode() const
 {
-    return { fsid(), 1 };
-}
-
-KResultOr<NonnullRefPtr<Inode>> DevPtsFS::create_inode(InodeIdentifier, const String&, mode_t, off_t, dev_t, uid_t, gid_t)
-{
-    return KResult(-EROFS);
-}
-
-KResult DevPtsFS::create_directory(InodeIdentifier, const String&, mode_t, uid_t, gid_t)
-{
-    return KResult(-EROFS);
+    return *m_root_inode;
 }
 
 RefPtr<Inode> DevPtsFS::get_inode(InodeIdentifier inode_id) const
@@ -175,11 +165,13 @@ RefPtr<Inode> DevPtsFSInode::lookup(StringView name)
     ASSERT(identifier().index() == 1);
 
     if (name == "." || name == "..")
-        return fs().get_inode(identifier());
+        return this;
+
+    auto& fs = static_cast<DevPtsFS&>(this->fs());
 
     auto pty_index = name.to_uint();
     if (pty_index.has_value() && ptys->contains(pty_index.value())) {
-        return fs().get_inode({ fsid(), pty_index_to_inode_index(pty_index.value()) });
+        return fs.get_inode({ fsid(), pty_index_to_inode_index(pty_index.value()) });
     }
 
     return {};
@@ -189,7 +181,12 @@ void DevPtsFSInode::flush_metadata()
 {
 }
 
-KResult DevPtsFSInode::add_child(InodeIdentifier, const StringView&, mode_t)
+KResult DevPtsFSInode::add_child(Inode&, const StringView&, mode_t)
+{
+    return KResult(-EROFS);
+}
+
+KResultOr<NonnullRefPtr<Inode>> DevPtsFSInode::create_child(const String&, mode_t, dev_t, uid_t, gid_t)
 {
     return KResult(-EROFS);
 }

Kernel/FileSystem/DevPtsFS.h
@@ -36,6 +36,8 @@ class SlavePTY;
 class DevPtsFSInode;
 
 class DevPtsFS final : public FS {
+    friend class DevPtsFSInode;
+
 public:
     virtual ~DevPtsFS() override;
     static NonnullRefPtr<DevPtsFS> create();
@@ -43,9 +45,7 @@ public:
     virtual bool initialize() override;
     virtual const char* class_name() const override { return "DevPtsFS"; }
 
-    virtual InodeIdentifier root_inode() const override;
-    virtual KResultOr<NonnullRefPtr<Inode>> create_inode(InodeIdentifier parent_id, const String& name, mode_t, off_t size, dev_t, uid_t, gid_t) override;
-    virtual KResult create_directory(InodeIdentifier parent_inode, const String& name, mode_t, uid_t, gid_t) override;
+    virtual NonnullRefPtr<Inode> root_inode() const override;
     virtual RefPtr<Inode> get_inode(InodeIdentifier) const override;
 
     static void register_slave_pty(SlavePTY&);
@@ -73,7 +73,8 @@ private:
     virtual RefPtr<Inode> lookup(StringView name) override;
     virtual void flush_metadata() override;
     virtual ssize_t write_bytes(off_t, ssize_t, const u8* buffer, FileDescription*) override;
-    virtual KResult add_child(InodeIdentifier child_id, const StringView& name, mode_t) override;
+    virtual KResultOr<NonnullRefPtr<Inode>> create_child(const String& name, mode_t, dev_t, uid_t, gid_t) override;
+    virtual KResult add_child(Inode&, const StringView& name, mode_t) override;
     virtual KResult remove_child(const StringView& name) override;
     virtual size_t directory_entry_count() const override;
     virtual KResult chmod(mode_t) override;

Kernel/FileSystem/Ext2FileSystem.h
@@ -63,7 +63,8 @@ private:
     virtual RefPtr<Inode> lookup(StringView name) override;
     virtual void flush_metadata() override;
     virtual ssize_t write_bytes(off_t, ssize_t, const u8* data, FileDescription*) override;
-    virtual KResult add_child(InodeIdentifier child_id, const StringView& name, mode_t) override;
+    virtual KResultOr<NonnullRefPtr<Inode>> create_child(const String& name, mode_t, dev_t, uid_t, gid_t) override;
+    virtual KResult add_child(Inode& child, const StringView& name, mode_t) override;
     virtual KResult remove_child(const StringView& name) override;
     virtual int set_atime(time_t) override;
     virtual int set_ctime(time_t) override;
@@ -128,9 +129,9 @@ private:
     bool flush_super_block();
 
     virtual const char* class_name() const override;
-    virtual InodeIdentifier root_inode() const override;
-    virtual KResultOr<NonnullRefPtr<Inode>> create_inode(InodeIdentifier parent_id, const String& name, mode_t, off_t size, dev_t, uid_t, gid_t) override;
-    virtual KResult create_directory(InodeIdentifier parent_inode, const String& name, mode_t, uid_t, gid_t) override;
+    virtual NonnullRefPtr<Inode> root_inode() const override;
+    KResultOr<NonnullRefPtr<Inode>> create_inode(InodeIdentifier parent_id, const String& name, mode_t, off_t size, dev_t, uid_t, gid_t);
+    KResult create_directory(InodeIdentifier parent_inode, const String& name, mode_t, uid_t, gid_t);
     virtual RefPtr<Inode> get_inode(InodeIdentifier) const override;
     virtual void flush_writes() override;
 

Kernel/FileSystem/FileSystem.h
@@ -56,7 +56,7 @@ public:
 
     virtual bool initialize() = 0;
     virtual const char* class_name() const = 0;
-    virtual InodeIdentifier root_inode() const = 0;
+    virtual NonnullRefPtr<Inode> root_inode() const = 0;
     virtual bool supports_watchers() const { return false; }
 
     bool is_readonly() const { return m_readonly; }
@@ -77,9 +77,6 @@ public:
         InodeIdentifier inode;
         u8 file_type { 0 };
     };
-
-    virtual KResultOr<NonnullRefPtr<Inode>> create_inode(InodeIdentifier parent_id, const String& name, mode_t, off_t size, dev_t, uid_t, gid_t) = 0;
-    virtual KResult create_directory(InodeIdentifier parent_inode, const String& name, mode_t, uid_t, gid_t) = 0;
 
     virtual RefPtr<Inode> get_inode(InodeIdentifier) const = 0;
 
@@ -110,11 +107,6 @@ inline FS* InodeIdentifier::fs()
 inline const FS* InodeIdentifier::fs() const
 {
     return FS::from_fsid(m_fsid);
-}
-
-inline bool InodeIdentifier::is_root_inode() const
-{
-    return (*this) == fs()->root_inode();
 }
 
 }

Kernel/FileSystem/Inode.h
@@ -72,7 +72,8 @@ public:
     virtual KResult traverse_as_directory(Function<bool(const FS::DirectoryEntry&)>) const = 0;
     virtual RefPtr<Inode> lookup(StringView name) = 0;
     virtual ssize_t write_bytes(off_t, ssize_t, const u8* data, FileDescription*) = 0;
-    virtual KResult add_child(InodeIdentifier child_id, const StringView& name, mode_t) = 0;
+    virtual KResultOr<NonnullRefPtr<Inode>> create_child(const String& name, mode_t, dev_t, uid_t, gid_t) = 0;
+    virtual KResult add_child(Inode&, const StringView& name, mode_t) = 0;
     virtual KResult remove_child(const StringView& name) = 0;
     virtual size_t directory_entry_count() const = 0;
     virtual KResult chmod(mode_t) = 0;

Kernel/FileSystem/InodeIdentifier.h
@@ -62,8 +62,6 @@ public:
         return m_fsid != other.m_fsid || m_index != other.m_index;
     }
 
-    bool is_root_inode() const;
-
     String to_string() const { return String::format("%u:%u", m_fsid, m_index); }
 
 private:

Kernel/FileSystem/ProcFS.cpp
@@ -1075,19 +1075,9 @@ const char* ProcFS::class_name() const
     return "ProcFS";
 }
 
-KResultOr<NonnullRefPtr<Inode>> ProcFS::create_inode(InodeIdentifier, const String&, mode_t, off_t, dev_t, uid_t, gid_t)
+NonnullRefPtr<Inode> ProcFS::root_inode() const
 {
-    return KResult(-EROFS);
-}
-
-KResult ProcFS::create_directory(InodeIdentifier, const String&, mode_t, uid_t, gid_t)
-{
-    return KResult(-EROFS);
-}
-
-InodeIdentifier ProcFS::root_inode() const
-{
-    return { fsid(), FI_Root };
+    return *m_root_inode;
 }
 
 RefPtr<Inode> ProcFS::get_inode(InodeIdentifier inode_id) const
@@ -1095,7 +1085,7 @@ RefPtr<Inode> ProcFS::get_inode(InodeIdentifier inode_id) const
 #ifdef PROCFS_DEBUG
     dbg() << "ProcFS::get_inode(" << inode_id.index() << ")";
 #endif
-    if (inode_id == root_inode())
+    if (inode_id == root_inode()->identifier())
         return m_root_inode;
 
     LOCKER(m_inodes_lock);
@@ -1340,7 +1330,7 @@ RefPtr<Inode> ProcFSInode::lookup(StringView name)
 {
     ASSERT(is_directory());
     if (name == ".")
-        return fs().get_inode(identifier());
+        return this;
     if (name == "..")
         return fs().get_inode(to_parent_id(identifier()));
 
@@ -1555,11 +1545,18 @@ InodeMetadata ProcFSProxyInode::metadata() const
     return metadata;
 }
 
-KResult ProcFSProxyInode::add_child(InodeIdentifier child_id, const StringView& name, mode_t mode)
+KResultOr<NonnullRefPtr<Inode>> ProcFSProxyInode::create_child(const String& name, mode_t mode, dev_t dev, uid_t uid, gid_t gid)
 {
     if (!m_fd->inode())
         return KResult(-EINVAL);
-    return m_fd->inode()->add_child(child_id, name, mode);
+    return m_fd->inode()->create_child(name, mode, dev, uid, gid);
+}
+
+KResult ProcFSProxyInode::add_child(Inode& child, const StringView& name, mode_t mode)
+{
+    if (!m_fd->inode())
+        return KResult(-EINVAL);
+    return m_fd->inode()->add_child(child, name, mode);
 }
 
 KResult ProcFSProxyInode::remove_child(const StringView& name)
@@ -1583,10 +1580,13 @@ size_t ProcFSProxyInode::directory_entry_count() const
     return m_fd->inode()->directory_entry_count();
 }
 
-KResult ProcFSInode::add_child(InodeIdentifier child_id, const StringView& name, mode_t)
+KResultOr<NonnullRefPtr<Inode>> ProcFSInode::create_child(const String&, mode_t, dev_t, uid_t, gid_t)
 {
-    (void)child_id;
-    (void)name;
+    return KResult(-EPERM);
+}
+
+KResult ProcFSInode::add_child(Inode&, const StringView&, mode_t)
+{
     return KResult(-EPERM);
 }
 

commit 260f872fde2322daf0dabb0ff15264bb70cb7163

Ext2FS: Fix inode link leak on all new inodes
The initial inode link count was wrong in Ext2FS, as the act of adding
new inodes to their new parent bumps the count.

This regressed in df66c28479e9206adeef1f40f8c0e448c8aea77e.

Kernel/FileSystem/Ext2FileSystem.cpp
@@ -1428,12 +1428,6 @@ KResultOr<NonnullRefPtr<Inode>> Ext2FS::create_inode(InodeIdentifier parent_id, 
     bool success = set_inode_allocation_state(inode_id, true);
     ASSERT(success);
 
-    unsigned initial_links_count;
-    if (is_directory(mode))
-        initial_links_count = 2; // (parent directory + "." entry in self)
-    else
-        initial_links_count = 1;
-
     struct timeval now;
     kgettimeofday(now);
     ext2_inode e2inode;
@@ -1446,7 +1440,9 @@ KResultOr<NonnullRefPtr<Inode>> Ext2FS::create_inode(InodeIdentifier parent_id, 
     e2inode.i_ctime = now.tv_sec;
     e2inode.i_mtime = now.tv_sec;
     e2inode.i_dtime = 0;
-    e2inode.i_links_count = initial_links_count;
+
+    // For directories, add +1 link count for the "." entry in self.
+    e2inode.i_links_count = is_directory(mode);
 
     if (is_character_device(mode))
         e2inode.i_block[0] = dev;